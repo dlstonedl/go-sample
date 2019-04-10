58a6d66f574fa92100acfd5e668fa4d2d0b46c09
dlstonedl <ldeng@thoughtworks.com>
2019-04-10 23:19:56 +0800

    parse city list

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # go-sample
 use go to implement simple function
+
+go get golang.org/x/text
+go get golang.org/x/net/html
---
+++ crawler/data/parser/citylist.go
@@ -0,0 +1,22 @@
+package parser
+
+import (
+	"github.com/dlstonedl/go-sample/crawler/engine"
+	"regexp"
+)
+
+var cityListRe = regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`)
+
+func ParseCityList(contents []byte) engine.ParseResult {
+	matches := cityListRe.FindAllSubmatch(contents, -1)
+
+	result := engine.ParseResult{}
+	for _, m := range matches {
+		result.Items = append(result.Items, string(m[2]))
+		result.Requests = append(result.Requests, engine.Request{
+			Url:       string(m[1]),
+			ParseFunc: engine.NilParse,
+		})
+	}
+	return result
+}

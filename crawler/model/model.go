85cc51b6af7eefc8d2d81ce8979e6e1c51fbe90c
dlstonedl <ldeng@thoughtworks.com>
2019-04-12 23:41:51 +0800

    model profile

---
+++ crawler/model/model.go
@@ -0,0 +1,16 @@
+package model
+
+type Profile struct {
+	Name          string
+	Gender        string
+	Age           int
+	Height        string
+	Weight        string
+	Income        string
+	Marriage      string
+	Education     string
+	House         string
+	Car           string
+	Residence     string
+	Constellation string
+}

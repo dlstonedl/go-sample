09e3cfa46258c35b737621bd382e68b8acc5b007
dlstonedl <ldeng@thoughtworks.com>
2019-04-14 16:08:40 +0800

    capture more data

--- crawler/zhenai/parser/city.go
+++ crawler/zhenai/parser/city.go
@@ -5,10 +5,13 @@ import (
 	"regexp"
 )
 
-var cityRe = regexp.MustCompile(`<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`)
+var (
+	profileRe = regexp.MustCompile(`<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`)
+	cityRe    = regexp.MustCompile(`href="(http://www.zhenai.com/zhenghun/[^"]+)"`)
+)
 
 func ParseCity(contents []byte) engine.ParseResult {
-	matches := cityRe.FindAllSubmatch(contents, -1)
+	matches := profileRe.FindAllSubmatch(contents, -1)
 
 	result := engine.ParseResult{}
 	for _, m := range matches {
@@ -21,5 +24,14 @@ func ParseCity(contents []byte) engine.ParseResult {
 			},
 		})
 	}
+
+	matches = cityRe.FindAllSubmatch(contents, -1)
+	for _, m := range matches {
+		result.Requests = append(result.Requests, engine.Request{
+			Url:       string(m[1]),
+			ParseFunc: ParseCity,
+		})
+	}
+
 	return result
 }

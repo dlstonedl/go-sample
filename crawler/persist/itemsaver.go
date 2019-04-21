bcfbe975ac74ec903201aabc408a1d9fce891d2d
dlstonedl <ldeng@thoughtworks.com>
2019-04-21 10:52:47 +0800

    refactor saver

--- crawler/persist/itemsaver.go
+++ crawler/persist/itemsaver.go
@@ -5,9 +5,25 @@ import (
 	"fmt"
 	"github.com/dlstonedl/go-sample/crawler/engine"
 	"github.com/olivere/elastic"
+	"log"
 )
 
+type SingleSaver struct {
+	Index    string
+	EsClient GetEsClientFunc
+}
+
 type GetEsClientFunc func() *elastic.Client
+
+func (s *SingleSaver) ItemSaver(item engine.Item) error {
+	err := save(s.EsClient(), s.Index, item)
+	if err != nil {
+		log.Printf("fail save %v\n", item)
+		return err
+	}
+
+	return nil
+}
 
 func save(client *elastic.Client, index string, item engine.Item) error {
 	if item.Type == "" || item.Id == "" {
